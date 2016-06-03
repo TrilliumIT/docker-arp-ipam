e258ed4d4dfd80e3605eec7d60294466c7ffa8c2
Clint Armstrong <clint@clintarmstrong.net>
2016-06-03 11:16:04 -0400

    impliment own logic for lastaddress and fix random address logic

--- driver/ipam.go
+++ driver/ipam.go
@@ -12,7 +12,6 @@ import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/docker/go-plugins-helpers/ipam"
 	"github.com/vishvananda/netlink"
-	"github.com/llimllib/ipaddress"
 
 	//"golang.org/x/net/context"
 	//dockerclient "github.com/docker/engine-api/client"
@@ -84,6 +83,7 @@ func (d *Driver) RequestAddress(r *ipam.RequestAddressRequest) (*ipam.RequestAdd
 	//FIXME checkneigh
 
 	if r.Address != "" {
+		log.Debugf("Specific Address Requested: %v", r.Address)
 		addr := net.ParseIP(r.Address)
 		if addr == nil {
 			log.Errorf("Unable to parse address: %v", r.Address)
@@ -105,20 +105,31 @@ func (d *Driver) RequestAddress(r *ipam.RequestAddressRequest) (*ipam.RequestAdd
 		return res, nil
 	}
 
+	log.Debugf("Random Address Requested in network %v", n)
 	triedAddresses := make(map[string]struct{})
 	var e struct{}
-	triedAddresses[string(ipaddress.LastAddress(n))] = e
-	triedAddresses[string(n.IP)] = e
+	lastAddress := lastAddr(n)
+	log.Debugf("Excluding last address: %v", lastAddress)
+	triedAddresses[string(lastAddress)] = e
+	firstAddress := firstAddr(n)
+	log.Debugf("Excluding network address: %v", firstAddress)
+	triedAddresses[string(firstAddress)] = e
 	ones, maskSize := n.Mask.Size()
 	var totalAddresses int
 	totalAddresses = 1<<uint8(maskSize - ones)
+	log.Debugf("Address avaliable to try: %v", totalAddresses)
 	for len(triedAddresses) < totalAddresses {
 		try, err := randAddr(n)
+		log.Debugf("Trying random address: %v", try)
 		if err != nil {
 			log.Errorf("Error generating random address: %v", err)
 			return nil, err
 		}
-		if _, ok := triedAddresses[string(try)]; ok { continue }
+		log.Debugf("Checking triedAddresses: %v", triedAddresses)
+		if _, ok := triedAddresses[string(try)]; ok { 
+			log.Debugf("Address already tried: %v", try)
+			continue
+		}
 
 		check, err := tryAddress(&try)
 		if err != nil {
@@ -127,31 +138,100 @@ func (d *Driver) RequestAddress(r *ipam.RequestAddressRequest) (*ipam.RequestAdd
 		}
 
 		if !check {
+			log.Debugf("Returning address: %v", try)
 			ret_addr := net.IPNet{ IP: try, Mask: n.Mask }
 			res.Address = ret_addr.String()
 			return res, nil
 		}
 		triedAddresses[string(try)] = e
+		log.Debugf("Address in use: %v", try)
 	}
 
 	log.Errorf("All avaliable addresses are in use")
 	return nil, fmt.Errorf("All avaliable addresses are in use")
 }
 
-func randAddr(n *net.IPNet) (net.IP, error) {
-	// ip & (mask | random) should generate a random ip
+func lastAddr(n *net.IPNet) net.IP {
 	ip := n.IP.To4()
 	if ip != nil {
+		log.Debugf("Type IPv4")
+		return net.IP{
+			ip[0] | ^n.Mask[0],
+			ip[1] | ^n.Mask[1],
+			ip[2] | ^n.Mask[2],
+			ip[3] | ^n.Mask[3],
+		}
+	}
+
+	return net.IP{
+		ip[0] | ^n.Mask[0],
+		ip[1] | ^n.Mask[1],
+		ip[2] | ^n.Mask[2],
+		ip[3] | ^n.Mask[3],
+		ip[4] | ^n.Mask[4],
+		ip[5] | ^n.Mask[5],
+		ip[6] | ^n.Mask[6],
+		ip[7] | ^n.Mask[7],
+		ip[8] | ^n.Mask[8],
+		ip[9] | ^n.Mask[9],
+		ip[10] | ^n.Mask[10],
+		ip[11] | ^n.Mask[11],
+		ip[12] | ^n.Mask[12],
+		ip[13] | ^n.Mask[13],
+		ip[14] | ^n.Mask[14],
+		ip[15] | ^n.Mask[15],
+	}
+}
+
+func firstAddr(n *net.IPNet) net.IP {
+	ip := n.IP.To4()
+	if ip != nil {
+		log.Debugf("Type IPv4")
+		return net.IP{
+			ip[0] & n.Mask[0],
+			ip[1] & n.Mask[1],
+			ip[2] & n.Mask[2],
+			ip[3] & n.Mask[3],
+		}
+	}
+
+	return net.IP{
+		ip[0] & n.Mask[0],
+		ip[1] & n.Mask[1],
+		ip[2] & n.Mask[2],
+		ip[3] & n.Mask[3],
+		ip[4] & n.Mask[4],
+		ip[5] & n.Mask[5],
+		ip[6] & n.Mask[6],
+		ip[7] & n.Mask[7],
+		ip[8] & n.Mask[8],
+		ip[9] & n.Mask[9],
+		ip[10] & n.Mask[10],
+		ip[11] & n.Mask[11],
+		ip[12] & n.Mask[12],
+		ip[13] & n.Mask[13],
+		ip[14] & n.Mask[14],
+		ip[15] & n.Mask[15],
+	}
+}
+
+func randAddr(n *net.IPNet) (net.IP, error) {
+	// ip & (mask | random) should generate a random ip
+	log.Debugf("Generating random address for network: %v", n)
+	ip := n.IP.To4()
+	if ip != nil {
+		log.Debugf("Type IPv4")
 		rand_bytes := make([]byte, 4)
 		_, err := rand.Read(rand_bytes)
+		log.Debugf("Random bytes: %v", rand_bytes)
 		if err != nil {
 			return nil, err
 		}
 		return net.IP{
-			ip[0] & (n.Mask[0] | rand_bytes[0]),
-			ip[1] & (n.Mask[1] | rand_bytes[1]),
-			ip[2] & (n.Mask[2] | rand_bytes[2]),
-			ip[3] & (n.Mask[3] | rand_bytes[3]),
+			ip[0] | (^n.Mask[0] & rand_bytes[0]),
+			ip[1] | (^n.Mask[1] & rand_bytes[1]),
+			ip[2] | (^n.Mask[2] & rand_bytes[2]),
+			ip[3] | (^n.Mask[3] & rand_bytes[3]),
 		}, nil
 	}
 
@@ -161,22 +241,22 @@ func randAddr(n *net.IPNet) (net.IP, error) {
 		return nil, err
 	}
 	return net.IP{
-		ip[0] & (n.Mask[0] | rand_bytes[0]),
-		ip[1] & (n.Mask[1] | rand_bytes[1]),
-		ip[2] & (n.Mask[2] | rand_bytes[2]),
-		ip[3] & (n.Mask[3] | rand_bytes[3]),
-		ip[4] & (n.Mask[4] | rand_bytes[4]),
-		ip[5] & (n.Mask[5] | rand_bytes[5]),
-		ip[6] & (n.Mask[6] | rand_bytes[6]),
-		ip[7] & (n.Mask[7] | rand_bytes[7]),
-		ip[8] & (n.Mask[8] | rand_bytes[8]),
-		ip[9] & (n.Mask[9] | rand_bytes[9]),
-		ip[10] & (n.Mask[10] | rand_bytes[10]),
-		ip[11] & (n.Mask[11] | rand_bytes[11]),
-		ip[12] & (n.Mask[12] | rand_bytes[12]),
-		ip[13] & (n.Mask[13] | rand_bytes[13]),
-		ip[14] & (n.Mask[14] | rand_bytes[14]),
-		ip[15] & (n.Mask[15] | rand_bytes[15]),
+		ip[0] | (^n.Mask[0] & rand_bytes[0]),
+		ip[1] | (^n.Mask[1] & rand_bytes[1]),
+		ip[2] | (^n.Mask[2] & rand_bytes[2]),
+		ip[3] | (^n.Mask[3] & rand_bytes[3]),
+		ip[4] | (^n.Mask[4] & rand_bytes[4]),
+		ip[5] | (^n.Mask[5] & rand_bytes[5]),
+		ip[6] | (^n.Mask[6] & rand_bytes[6]),
+		ip[7] | (^n.Mask[7] & rand_bytes[7]),
+		ip[8] | (^n.Mask[8] & rand_bytes[8]),
+		ip[9] | (^n.Mask[9] & rand_bytes[9]),
+		ip[10] | (^n.Mask[10] & rand_bytes[10]),
+		ip[11] | (^n.Mask[11] & rand_bytes[11]),
+		ip[12] | (^n.Mask[12] & rand_bytes[12]),
+		ip[13] | (^n.Mask[13] & rand_bytes[13]),
+		ip[14] | (^n.Mask[14] & rand_bytes[14]),
+		ip[15] | (^n.Mask[15] & rand_bytes[15]),
 	}, nil
 }
 
